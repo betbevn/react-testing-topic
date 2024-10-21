8afa0925fbdbaf3a95d74ed94b01b429811dbd47
Hoang Nguyen <joseph@Hoangs-MacBook-Pro-3.local>
2024-10-21 16:31:41 +0700

    write tests for frontend

--- jest-with-react-testing-library/src/section2/divide.ts
+++
@@ -1,6 +0,0 @@
-export class ZeroDivisorError extends Error {}
-
-export function divide(dividend: number, divisor: number) {
-  if (divisor === 0) throw new ZeroDivisorError("Can not divide to 0");
-  return dividend / divisor;
-}
--- jest-with-react-testing-library/src/section3/mock_spy.ts
+++
@@ -1,5 +0,0 @@
-export class Calculator {
-  sum(a: number, b: number): number {
-    return a + b;
-  }
-}
